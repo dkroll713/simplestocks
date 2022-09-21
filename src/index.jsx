da998c7d9c2ff0cf922f8652e309524854bdaf70
David Kroll <dkroll713@gmail.com>
2022-09-21 14:30:49 -0500

    working chartjs charts

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 // Bring React in to build a component.
 import React from "react";
 // Import from react-dom the ability to create a root render
-import { createRoot } from "react-dom/client";
+import reactDom from "react-dom";
 import {
   BrowserRouter,
   Routes,
@@ -15,21 +15,27 @@ import HomePage from './components/HomePage/HomePage.jsx'
 import './_global.scss'
 
 // create the root of the app by selection where the app should be mounted in the dom
-const root = createRoot(document.getElementById("root"));
+const root = document.getElementById("root");
 
 // Here is out base App component.
 // Notice we are NOT using jsx here. This is because we have not set up babel yet.
-const App = () => {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <p>Hi</p>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 };
 
-root.render(<App />);
+reactDom.render(<App />, document.getElementById('root'));
