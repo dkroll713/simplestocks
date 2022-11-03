d4756491f383df54a3d2796bbb332aafc96369e1
David Kroll <dkroll713@gmail.com>
2022-11-03 17:00:02 -0500

    working tooltip functionality

---
+++ src/components/DetailedView/Views/OverviewViews/InfoComponent.jsx
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+
+const axios = require('axios');
+
+import store from '../../../../zs.js';
+import infoStore from './info.js'
+
+const InfoComponent = (props) => {
+  const [open, setOpen] = useState(false)
+
+  const openTT = (e) => {
+    setOpen(true)
+  }
+
+  const closeTT = (e) => {
+    setOpen(false)
+  }
+
+  return (
+    <>
+    <div className="infoComponent">
+      <p onMouseEnter={openTT} onMouseLeave={closeTT}>{props.title}: <span>{props.value}</span></p>
+      {
+        open ? (
+          <div className="tooltip">{props.desc}</div>
+        ) : null
+      }
+    </div>
+    </>
+  )
+}
+
+export default InfoComponent;
